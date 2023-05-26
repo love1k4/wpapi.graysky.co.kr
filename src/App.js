0b207fdb9a9e4acdafc30c196065c3eb15cdce8b
Edward <love1k4@naver.com>
2023-05-26 23:52:40 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Posts />} />       
+        <Route path="/" element={<Posts />} />
       </Routes>
     </BrowserRouter>
   );
